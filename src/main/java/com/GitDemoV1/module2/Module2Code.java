bf5ff75cd014b37f16294f2e73bc53974abb660f
Rajib3811 <irajibul088@gmail.com>
2024-10-16 14:20:22 -0400

    My First Commit

---
+++ src/main/java/com/GitDemoV1/module2/Module2Code.java
@@ -0,0 +1,9 @@
+package com.GitDemoV1.module2;
+
+public class Module2Code {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/main/java/com/GitDemoV1/module3/Module3Code.java
@@ -0,0 +1,9 @@
+package com.GitDemoV1.module3;
+
+public class Module3Code {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
